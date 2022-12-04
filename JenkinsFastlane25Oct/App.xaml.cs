8fc4bab758f8111c2f156bbf7d18e83dfe189716
Sumit <susingh@dminc.com>
2022-12-04 11:41:20 +0530

    test9 commit

--- JenkinsFastlane25Oct/App.xaml.cs
+++ JenkinsFastlane25Oct/App.xaml.cs
@@ -9,7 +9,7 @@ namespace JenkinsFastlane25Oct
         public App()
         {
             InitializeComponent();
-            //test
+            //test18
             MainPage = new MainPage();
         }
 
